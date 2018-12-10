2af9814e28a97ca8290767d2cd573eab34988b7a
amandatvt <amandatavares@ajatdesenvolvimento.com.br>
2018-12-10 00:39:48 -0200

    add comments to Binary tree final

--- arvoreBinaria/arvore.h
+++ arvoreBinaria/arvore.h
@@ -3,6 +3,7 @@
 #define TRUE 1
 #define FALSE 0
 
+// arvore possui um vertice (num) e no da esquerda e da direita
 typedef struct No Arvore;
 struct No{
     int num;
@@ -10,11 +11,17 @@ struct No{
     Arvore *dir;
 };
 
+// recebe a raiz da arvore. se for nulo, esta vazio
 int arvoreVazia(Arvore *raiz){
     if(raiz == NULL) return TRUE;
     else return FALSE;
 }
 
+// recebe uma referencia para a arvore e um inteiro que sera o valor do no
+// se nulo nao tem ainda, dai cria o nó, com esquerda, direita e vertice
+// se ja existia arvore e o numero for menor, insere na esquerda
+// se for maior, insere na direita
+// a arvore binaria é ordenada
 Arvore *inserir (Arvore *aux, int num){
     if(aux == NULL){
         aux = malloc(sizeof(Arvore));
@@ -29,6 +36,9 @@ Arvore *inserir (Arvore *aux, int num){
     return aux;
 }
 
+// recebe a arvore, o numero do vertice a pesquisar e um bool, que aqui é int
+// se a arvore existir e o vertice nao tiver sido encontrado, procura-se
+// codigo recursiva
 int consultar(Arvore *aux, int num, int achou){
     if(aux != NULL && achou == 0){
         if(aux->num == num){
@@ -41,6 +51,7 @@ int consultar(Arvore *aux, int num, int achou){
     }return achou;
 }
 
+// para mostrar em ordem, pegamos o no da esquerda, da raiz e depois da direita
 void arvoreEmOrdem(Arvore *aux){
     if(aux != NULL){
         arvoreEmOrdem(aux->esq);
@@ -49,6 +60,7 @@ void arvoreEmOrdem(Arvore *aux){
     }
 }
 
+// para mostrar em pre ordem, pegamos, o no da raiz esquerda, depois da direita
 void arvorePreOrdem(Arvore *aux){
     if(aux != NULL){
         printf("%d ", aux->num);
@@ -57,6 +69,7 @@ void arvorePreOrdem(Arvore *aux){
     }
 }
 
+// para mostrar em pos ordem, pegamos, o no da raiz esquerda, da direita e depois da raiz
 void arvorePosOrdem(Arvore *aux){
     if(aux != NULL){
         arvorePosOrdem(aux->esq);
@@ -65,33 +78,40 @@ void arvorePosOrdem(Arvore *aux){
     }
 }
 
+// a remocao é a parte mais complexa da arvore
+//criamos dois Nos auxiliares, p e p2
+// procuramos o no
+
 Arvore *remover(Arvore *aux, int num){
     Arvore *p, *p2;
     if(aux->num == num){
         if(aux->esq == aux->dir) {
-            //raiz nï¿½o tem filhos
+            //raiz não tem filhos
             free(aux);
             return NULL;
         }
         else if(aux->esq == NULL){
-            // raiz nï¿½o tem filho para a esquerda
+            // raiz não tem filho para a esquerda
             p = aux->dir;
             free(aux);
             return p;
         }
         else if(aux->dir == NULL){
-            // raiz nï¿½o tem filho para a direita
+            // raiz não tem filho para a direita
             p = aux->esq;
             free(aux);
             return p;
         }
         else{
             //raiz tem filho para ambos os lados
+            // coloca o no direito em cada auxiliar
+            // se houver no esquerdo, um aux sera ele
             p2 = aux->dir;
             p = aux->dir;
             while(p->esq != NULL){
                 p = p->esq;
             }
+            // TODO: entender essa parte
             p->esq = aux->esq;
             free(aux);
             return p2;
@@ -115,6 +135,7 @@ Arvore *desalocar(Arvore *aux){
     return NULL;
 }
 
+// mostra os nos com seus filhos ou nao
 void mostrarNos(Arvore *aux){
     int r, d=0, e=0;
     if(aux != NULL){
